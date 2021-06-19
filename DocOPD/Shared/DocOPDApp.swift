3ce8d721a88abbba6b58eafc4c03eee6e67cb57e
Puneet Rangnekar <puneet.rangnekar@goplayfora.com>
2021-06-19 17:35:00 +0530

    Project folder added

---
+++ DocOPD/Shared/DocOPDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocOPDApp.swift
+//  Shared
+//
+//  Created by Puneet on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DocOPDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
