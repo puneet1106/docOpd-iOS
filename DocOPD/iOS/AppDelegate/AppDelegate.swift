1287d4e16a2d7ce3d2b28375f05215f1c9428537
Puneet Rangnekar <puneet.rangnekar@goplayfora.com>
2021-06-21 01:26:11 +0530

    .DS_Store banished!

---
+++ DocOPD/iOS/AppDelegate/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DocOPD (iOS)
+//
+//  Created by Puneet on 19/06/21.
+//
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        let nvc: UINavigationController = UINavigationController(rootViewController: initialViewController)
+        appDelegate?.window?.rootViewController = nvc
+        appDelegate?.window?.makeKeyAndVisible()
+        return true
+    }
+    
+    
+}
+
